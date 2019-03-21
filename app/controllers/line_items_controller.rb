49fdcd11d06bff8ac09c18deba27559f64c748e8
Tira Sundara <sundaralinus@gmail.com>
2019-03-21 21:13:13 +0700

    Add smarter cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,11 +28,11 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart, notice: 'Line item was successfully added.' }
+      if @line_item.save
+        format.html { redirect_to @cart, notice: 'Item was successfully added.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
