125966102c61ae8a98aace1cbe24c65db4aa848c
Tira Sundara <sundaralinus@gmail.com>
2019-03-21 20:19:04 +0700

    Add basic cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+	before_destroy :ensure_not_referenced_by_any_line_item
+	has_many :line_items
+
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates_uniqueness_of :title
@@ -6,4 +9,13 @@ class Product < ApplicationRecord
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+	private
+
+	  def ensure_not_referenced_by_any_line_item
+	  	if line_items.present?
+	  		errors.add(:base, 'Line Item present')
+	  		throw(:abort)
+	  	end
+	  end
 end
